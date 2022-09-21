21472a8416d642ca3c042a3441d0b7209b324a2d
lufie <lufie@192.168.18.2>
2022-09-21 17:53:54 +0800

    remove rubbish

--- a.py
+++
@@ -1,9 +0,0 @@
-def tag(tag_name):
-    def add_tag(content):
-        return "<{0}>{1}</{0}>".format(tag_name, content)
-    return add_tag
-    
-content = 'Hello'
-a = tag('a')
-b = a('aaa')
-print(b)
